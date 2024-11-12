26411089ba2d7634ef91d485a33182b7e513cc83
jamess-nguyen <haimenguyen@gmail.com>
2024-11-11 19:01:35 -0800

    :sparkles: added front end htmls for generate and driver

--- brutas_combinations/urls.py
+++ brutas_combinations/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('generate/', views.generate_combinations, name='generate_combinations'),
-    path('user_combinations/', views.display_combinations, name='display_combinations'),
+    path('combinations/', views.display_combinations, name='display_combinations'),
+    path('driver/', views.web_driver, name='selenium_driver'),
+
 ]
--- brutas_combinations/views.py
+++ brutas_combinations/views.py
@@ -5,16 +5,19 @@ from django.utils import timezone
 
 from .models import UserCombination
 
-def generate_combinations(request):
-    # Generate combinations for numbers 1 to 500
+def generate_user_pass():
     temp_combinations=[]
     for num in range(1, 501):
         num_str = str(num)  # Convert number to string
         temp_combinations.append(num_str)
         # Store the combination and the current datetime in the database
         UserCombination.objects.create(username_str="Temp_UserName", password_str=num_str, created_at=timezone.now())
-    
-    return HttpResponse(f"User/Password combination(s) generated and stored successfully. {temp_combinations}")
+
+def generate_combinations(request):
+     if request.method == "POST":
+        generate_user_pass()
+
+     return render(request, 'gen_combination/generate.html')
 
 
 def display_combinations(request):
@@ -27,3 +30,12 @@ def display_combinations(request):
 
     return render(request, 'user_combinations/display_user_combinations.html', {'page_obj': page_obj})
 
+def selenium_driver():
+    return
+
+def web_driver(request):
+    if request.method == "POST":
+        selenium_driver()
+        return render(request, 'web_driver/driver.html', {'success': True})
+    
+    return render(request, 'web_driver/driver.html')   
