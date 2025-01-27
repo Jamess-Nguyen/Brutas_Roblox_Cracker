7be1c7430ff25b5043ea8d5c44249cd9744de78b
jamess-nguyen <haimenguyen@gmail.com>
2025-01-26 16:48:37 -0800

    :sparkles: wrote primitive backtracking

--- combinations.py
+++ combinations.py
@@ -1,6 +1,16 @@
-combinations = [
-    ("user1", "password1")
-]
+def combinations(username, password, idx, candiates):
+    if idx>=len(candidates):
+        print(f"{username}\t{password}")
+        return
+    
+    skip = combinations(username, password, idx+1, candidates)
+    password = password+candidates[idx]
+    select = combinations(username, password, idx+1, candidates)
 
-for username, password in combinations:
-    print(f"{username}\t{password}")
+
+if __name__ == "__main__":
+    username = "test123"
+    candidates = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+    combinations(username, "", 0, candidates)
+
